dddc28d3aee1ee52ffa6af2fe8c7a6e61741f843
Lucas Chacon <lucaschacon37@gmail.com>
2025-09-15 13:40:09 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -25,7 +25,6 @@ public class Category implements Serializable {
 	private String name;
 	
 	// ACRESCENTANDO O ATRIBUTO PARA ASSOCIAÇÃO DOS PRODUTOS COM AS CATEGORIAS:
-	
 	@JsonIgnore
 	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -2,8 +2,9 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -27,13 +29,16 @@ public class Product implements Serializable{
 	private Double price;
 	private String imgUrl;
 	
-	// 'SET' REPRESENTA UM CONJUNTO QUE GARANTE QUE NÃO VOU TER UM PRODUTO COM MAIS
-	// DE UMA OCORRENCIA DA MESMA CATEGORIA
+
+	// 'SET' REPRESENTA UM CONJUNTO QUE GARANTE QUE NÃO VOU TER UM PRODUTO COM MAIS DE UMA OCORRENCIA DA MESMA CATEGORIA
 	@ManyToMany
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
-	private Set<Category> categories = new HashSet<>(); // INSTANCIAR PARA GARANTIR QUE A COLEÇÃO Ñ COMECE VALENDO NULA
-														// E SIM VAZIA
-
+	private Set<Category> categories = new HashSet<>(); 
+	// INSTANCIAR PARA GARANTIR QUE A COLEÇÃO Ñ COMECE VALENDO NULA E SIM VAZIA
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Product() {
 	}
 
@@ -89,10 +94,22 @@ public class Product implements Serializable{
 	public Set<Category> getCategories() {
 		return categories;
 	}
+	
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
+	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -104,6 +121,11 @@ public class Product implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Product other = (Product) obj;
-		return Objects.equals(id, other.id);
-	}
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
 }
