1a7d9b0976ec139f96a89c955c19c262e2abf961
Lucas Chacon <lucaschacon37@gmail.com>
2025-09-15 11:07:44 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -9,8 +9,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
@@ -25,10 +27,13 @@ public class Product implements Serializable{
 	private Double price;
 	private String imgUrl;
 	
-	// 'SET' REPRESENTA UM CONJUNTO QUE GARANTE QUE NÃO VOU TER UM PRODUTO COM MAIS DE UMA OCORRENCIA DA MESMA CATEGORIA
-	@Transient
-	private Set<Category> categories = new HashSet<>(); // INSTANCIAR PARA GARANTIR QUE A COLEÇÃO Ñ COMECE VALENDO NULA E SIM VAZIA
-	
+	// 'SET' REPRESENTA UM CONJUNTO QUE GARANTE QUE NÃO VOU TER UM PRODUTO COM MAIS
+	// DE UMA OCORRENCIA DA MESMA CATEGORIA
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>(); // INSTANCIAR PARA GARANTIR QUE A COLEÇÃO Ñ COMECE VALENDO NULA
+														// E SIM VAZIA
+
 	public Product() {
 	}
 
